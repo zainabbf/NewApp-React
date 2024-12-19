2ca4dc3b7b76dd3991555790fe31dba428eaaeda
zainabbf <zainab.hassan.217@gmail.com>
2024-12-19 14:47:28 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
 <>
-    <h2>New Branch - new Version</h2>
+    <h2> new Version</h2>
     <h1 className="title">Reboot01</h1>
     <h2 className="title">Zainab Hasan</h2>
 </>
