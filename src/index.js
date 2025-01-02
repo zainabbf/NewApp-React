af27dc35432f20f8212c70f604442eece2b791b8
zainabbf <zainab.hassan.217@gmail.com>
2025-01-02 14:25:38 +0300

    change the version with update the package.json and change dir of pages folder and deal with pages with router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/home';
+import Gallery from './pages/gallery';
+import Text from './pages/linktext'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1 style={{ textAlign: 'center' }}>The page is not found 404 </h1>,
+  },
+
+  {
+    path: "/gallery",
+    element: <Gallery />,
+  },
+
+  {
+    path: "/linktext",
+    element: <Text />,
+  },
+]);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <RouterProvider router={router} />
   </React.StrictMode>
 );
 
